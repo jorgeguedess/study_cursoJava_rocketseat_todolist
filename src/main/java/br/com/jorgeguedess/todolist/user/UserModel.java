412341b812fe29ce6c52d840ab01010cc6107b78
jorgeguedess <jorge_guedes00@hotmail.com>
2023-10-10 22:57:11 -0300

    feat: add new form get and setters

--- src/main/java/br/com/jorgeguedess/todolist/user/UserModel.java
+++ src/main/java/br/com/jorgeguedess/todolist/user/UserModel.java
@@ -1,33 +1,10 @@
 package br.com.jorgeguedess.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
   public String username;
   public String name;
   public String password;
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  // getters setters
 }
